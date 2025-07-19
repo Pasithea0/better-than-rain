345729ae5ee76959a50e35a8ad46e7dddc4e457f
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-07-19 17:02:12 -0600

    init sounds

--- src/main/java/com/pasithea0/betterthanrain/BetterThanRainMod.java
+++ src/main/java/com/pasithea0/betterthanrain/BetterThanRainMod.java
@@ -1,6 +1,9 @@
 package com.pasithea0.betterthanrain;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.sound.SoundRepository;
+import net.minecraft.core.sound.SoundTypes;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
@@ -8,13 +11,13 @@ import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class BetterThanRainMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+public class BetterThanRainMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint, ClientStartEntrypoint {
     public static final String MOD_ID = "betterthanrain";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Better than Rain! mod initialized.");
+        LOGGER.info("✅ Better than Rain! mod initialized.");
     }
 
 	@Override
@@ -29,11 +32,22 @@ public class BetterThanRainMod implements ModInitializer, RecipeEntrypoint, Game
 
 	@Override
 	public void beforeGameStart() {
-
+		SoundTypes.loadSoundsJson(MOD_ID);
 	}
 
 	@Override
 	public void afterGameStart() {
+
+	}
+
+	@Override
+	public void beforeClientStart() {
+		SoundRepository.registerNamespace(MOD_ID);
+		LOGGER.info("✅ Better than Rain! client initialized.");
+	}
+
+	@Override
+	public void afterClientStart() {
 
 	}
 }
